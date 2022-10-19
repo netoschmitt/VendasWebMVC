41e4b429113cec5cd30dd728c1f4b5adf80a8748
neto schmitt <agneloschmittneto@gmail.com>
2022-10-19 19:24:59 -0300

    Implementando modelo de dominio - Atributos basicos

---
+++ VendasWebMVC/Models/Enums/SaleStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMvc.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0, // pendente
+        Billed = 1, // faturado
+        Canceled = 2 // cancelado
+    }
+}
